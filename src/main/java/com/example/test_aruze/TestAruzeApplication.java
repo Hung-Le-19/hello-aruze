d29131bdc71818c7f8d213ba383dafafa40bdffd
hungl <hungle19052001@gmail.com>
2023-02-19 22:44:18 +0700

    first commit

---
+++ src/main/java/com/example/test_aruze/TestAruzeApplication.java
@@ -0,0 +1,18 @@
+package com.example.test_aruze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class TestAruzeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAruzeApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String message(){
+        return "Hello Hungle";
+    }
+}
---
+++ src/test/java/com/example/test_aruze/TestAruzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_aruze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAruzeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
